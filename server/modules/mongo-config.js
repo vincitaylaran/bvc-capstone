a6f2f3422b7f47bfe5a818f85f72ed9ff5711db5
Vinci Taylaran <svl.taylaran@gmail.com>
2020-11-17 10:15:40 -0700

    Update getConnectionUri to return environment variable MONGODB_URI

--- server/modules/mongo-config.js
+++ server/modules/mongo-config.js
@@ -3,15 +3,16 @@ const config = require("../config/config.json")
 /// This class handles the construction of connection credentials.
 module.exports = class MongoConfig {
   static getConnectionUri() {
-    const HOST = config.MongoDbAtlasDatabaseHost
-    const USERNAME = config.MongoDbAtlasUsername
-    const PASSWORD = config.MongoDbAtlasPassword
-    const SERVER = config.MongoDbAtlasDatabaseServer
-    const DB_NAME = config.MongoDbAtlasDatabaseName
-    const QUERY_STRING = config.MongoDbAtlasQueryString
+    // const HOST = config.MongoDbAtlasDatabaseHost
+    // const USERNAME = config.MongoDbAtlasUsername
+    // const PASSWORD = config.MongoDbAtlasPassword
+    // const SERVER = config.MongoDbAtlasDatabaseServer
+    // const DB_NAME = config.MongoDbAtlasDatabaseName
+    // const QUERY_STRING = config.MongoDbAtlasQueryString
 
-    return HOST + USERNAME + ":" + PASSWORD + SERVER + DB_NAME + QUERY_STRING
+    // return HOST + USERNAME + ":" + PASSWORD + SERVER + DB_NAME + QUERY_STRING
 
-    return config.MongoConnection
+    // return config.MongoConnection
+    return process.env.MONGODB_URI
   }
 }
